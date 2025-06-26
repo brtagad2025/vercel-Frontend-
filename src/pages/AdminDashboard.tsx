5771de62967dde734584fee7c651f97b306c41ca
Tagadplatforms <aniket.b@brtagad.com>
2025-06-26 18:47:25 +0530

    Update AdminDashboard.tsx

--- src/pages/AdminDashboard.tsx
+++ src/pages/AdminDashboard.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
+import ChatbotWidget from '../components/ChatbotWidget';
 import { 
   Users, 
   MessageSquare, 
@@ -326,6 +327,7 @@ const AdminDashboard = () => {
         </div>
       )}
     </div>
+    <ChatbotWidget />
   );
 };
 
